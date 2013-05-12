62606a1b87a89977a35481bed8fdfa4b11a51e5f
Genghua Li <genghuali@yahoo.com>
2013-05-11 22:03:23 -0600

    ready to deploy

---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+  test "that /login routes opens the login page" do
+  	get '/login'
+  	assert_response:success
+  end
+
+test "that /logout routes opens the logout page" do
+  	get '/logout'
+  	assert_response:redirect
+  	assert_redirected_to '/'
+  end
+  test "that /register routes opens the register page" do
+  	get '/register'
+  	assert_response:success
+  end
+end
